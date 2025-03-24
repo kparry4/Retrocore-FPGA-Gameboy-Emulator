896230c9a3d8b80414b9f002a183d40d93c10172
Ruslana Fogler <rfogler@ece012.ece.local.cmu.edu>
2025-03-24 11:33:39 -0400

    changed run-doc and added bash script to replace mif files

---
+++ tests/integration/replace_mif.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if correct number of arguments is provided
+if [ "$#" -ne 1 ]; then
+    echo "Please give path to binary file name $0 <new_init_file_path> you beautiful individual"
+    exit 1
+fi
+
+ROM_PATH="../../code/Integration/MMU/BRAMS/ROM_BANK.v"
+NEW_FILE_NAME="$1"
+NEW_PATH="../../code/Integration/MMU/mif_files/$NEW_FILE_NAME.mif"
+
+# Use sed to replace the init_file path
+sed -i "s#\(altsyncram_component\.init_file = \).*#\1\"$NEW_PATH\";#" "$ROM_PATH"
+
+echo "Updated altsyncram_component.init_file in rom bank at path $ROM_PATH to be $NEW_PATH"
