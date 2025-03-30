3add7bdbe367f88852894705e4021930da483378
Katherine Parry <kparry4@gmail.com>
2025-03-30 14:04:58 -0400

    contoller top module update

--- tests/controller-test/controllertop.sv
+++ tests/controller-test/controllertop.sv
@@ -1,10 +1,14 @@
 module controllertop(
 	output logic [8:0] LEDG,
-	input logic [3:0] KEY
+	input logic [35:0] GPIO
 );
-	assign LEDG[0] = KEY[0];
-	assign LEDG[1] = KEY[1];
-	assign LEDG[2] = KEY[2];
-	assign LEDG[3] = KEY[3];
+	assign LEDG[0] = GPIO[0];
+	assign LEDG[1] = GPIO[1];
+	assign LEDG[2] = GPIO[2];
+	assign LEDG[3] = GPIO[3];
+	assign LEDG[4] = GPIO[4];
+	assign LEDG[5] = GPIO[5];
+	assign LEDG[6] = GPIO[6];
+	assign LEDG[7] = GPIO[7];
 
 endmodule
