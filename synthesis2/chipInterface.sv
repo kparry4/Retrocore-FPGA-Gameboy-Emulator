56c5f04b096ff65cab5e7d7914af11a267bfa139
Ruslana Fogler <rfogler@ece012.ece.local.cmu.edu>
2025-04-12 12:40:13 -0400

    forgot this

--- synthesis2/chipInterface.sv
+++ synthesis2/chipInterface.sv
@@ -26,7 +26,7 @@ module chipInterface(
     //inputs into gameboy
     logic rst, unsync_reset;
 	 logic vga_clock;
-    assign unsync_reset = ~KEY[0];
+    assign unsync_reset = ~KEY[2] || SW[0];
     logic [7:0] LY;
 
 
@@ -44,6 +44,7 @@ module chipInterface(
     logic [1:0] frame_pixel;
     logic frame_pixel_valid;
 	 logic[7:0] LCDC_R;
+	 logic [15:0] pc, npc;
 	 logic [1:0] ppu_mode;
 	 logic vga_wren;
 
@@ -52,9 +53,12 @@ module chipInterface(
     assign LY = 8'd01;
 
     assign LEDR[7:0] = GPIO[7:0];
-    assign LEDR[17:8] = '0;
-    assign LEDG = frame_pixel_valid;
-	 
+    assign LEDR[16:8] = '0;
+    assign LEDG[0] = frame_pixel_valid;
+	 assign LEDG[1] = rst;
+	 assign LEDG[8:2] = '0;
+	 assign LEDR[17] = rst;
+
 	 assign vga_clock = CLOCK3_50;
 
     always_ff @(posedge CLOCK3_50) begin
@@ -68,7 +72,7 @@ module chipInterface(
     integer pixel_count;
 
     integer r, c;
-	 
+
 
 
     vga #(20) v1(.CLOCK_50(vga_clock), .row(vga_row), .col(vga_col), .HS(VGA_HS), .VS(VGA_VS), .blank, .reset());
@@ -93,8 +97,8 @@ module chipInterface(
             vga_color = 24'hFF_00_00;
         end
 	 end
-	  
-	  
+
+
 	  always_comb begin
        case (frame_buffer_pixel)
               2'b11: vga_color_pixel = {8'd0,8'd0,8'd0};
@@ -104,44 +108,53 @@ module chipInterface(
 				  default: vga_color_pixel = 24'h00_FF_00;
         endcase
     end
-	 
+
 	 logic ppu_clk=0;
-	 
+
 	  always_ff @(posedge CLOCK_50) begin
-		 // if (rst) begin
-		 //   cpu_clk = clk;
-		 //   cnt = 0;
-		 //   //cpu_ppu = 0;
-		 // end
 		 ppu_clk <= ~ppu_clk;
-	  end	 
+	  end
 
 
 
 
     gameboy dut (.clk (CLOCK_50), //8Mhz
                 .clk2(ppu_clk), //4Mhz
-                .*);
-					 
+					 .rst,
+					 .LY,
+					 .joypad_select(1'b0),
+					 .joypad_start(1'b0),
+					 .joypad_dpad_up(1'b0),
+					 .joypad_dpad_down(1'b0),
+					 .joypad_dpad_left(1'b0),
+					 .joypad_dpad_right(1'b0),
+					 .joypad_a_button(1'b0),
+					 .joypad_b_button(1'b0),
+					 .frame_pixel,
+					 .frame_pixel_valid,
+					 .LCDC_R,
+					 .ppu_mode);
+
 
      FRAME_BUFFER frame ( .rdaddress( GET_FRAME_ADDR(vga_row / 3, vga_col / 3) ),
                          .rdclock(CLOCK3_50), //50 Mhz
                          .q(frame_buffer_pixel),
-								 
+
                          .wraddress( GET_FRAME_ADDR(r, c) ),
                          .wrclock(ppu_clk), //4 Mhz
                          .wren(vga_wren),
                          .data(frame_pixel));
 
     SevenSegmentDisplayWithHex hi (
-            .BCD7('0),
-            .BCD6('0),
-            .BCD5(vga_color[23:20]),
-            .BCD4(vga_color[19:16]),
-            .BCD3(vga_color[15:12]),
-            .BCD2(vga_color[11:8]),
-            .BCD1(vga_color[7:4]),
-            .BCD0(vga_color[3:0]),
+            .BCD7(npc[15:12]),
+            .BCD6(npc[11:8]),
+            .BCD5(npc[7:4]),
+            .BCD4(npc[3:0]),
+				
+            .BCD3(pc[15:12]),
+            .BCD2(pc[11:8]),
+            .BCD1(pc[7:4]),
+            .BCD0(pc[3:0]),
             .HEX7, .HEX6, .HEX5, .HEX4, .HEX3, .HEX2, .HEX1, .HEX0);
 
     always_ff @(posedge ppu_clk) begin
@@ -156,9 +169,9 @@ module chipInterface(
                 if(r < `HEIGHT) begin
                     pixel_count <= pixel_count + 1;
 						  vga_wren <= '1;
-                end else begin 
+                end else begin
 						  vga_wren <= '0;
-					 end 
+					 end
             end
         end
     end
@@ -166,3 +179,4 @@ module chipInterface(
 
 
 endmodule: chipInterface
+
